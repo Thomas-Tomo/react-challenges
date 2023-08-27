a454f1a573da0403f9de6a2d2f6ef1923bee0ea5
Thomas-Tomo <thomas.tdomitrovic@gmail.com>
2023-08-27 19:06:53 +0000

    Add PostItemAPI and ContentAPI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,20 @@
-import css from './App.module.css';
-import Sidebar from './components/Sidebar';
-// import NavBarSimple from './components/NavBarSimple';
-import NavBarForm from './components/NavBarForm';
-import Content from './components/Content';
-import ContentHooks from './components/ContentHooks';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+import ContentAPI from "./components/ContentAPI";
 
 function App() {
   return (
     <div className={css.App}>
-      <Sidebar />
+      {/* Add your components here */}
       <NavBarForm />
-      <ContentHooks />
+      <Sidebar />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      <ContentAPI />
     </div>
   );
 }
